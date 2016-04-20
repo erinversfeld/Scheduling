3141c146ea5763ae805f42659526fa4ba0ceb319
Erin <erinversfeld@gmail.com>
2016-04-20 15:21:11 +0200

    Revert "[refactor] rewrote the simulatorclock.toString to include all output in a single method call."
    This reverts commit e3b29dce10244694ecfd4780cadf20404be30a02.

--- src/simulator/SimulationClock.java
+++ src/simulator/SimulationClock.java
@@ -1,27 +1,27 @@
-package simulator;
+package simulator; 
 //
 import java.util.HashMap;
 import java.util.Map;
 /**
  * Implementation of SystemTimer type.
- *
+ * 
  * @author Stephan Jamieson
  * @version 8/3/2015
  */
 class SimulationClock implements EventHandler<TimeOutEvent>, SystemTimer {
-
+    
     private final int SYSCALL_COST;
     private final int CSWITCH_COST;
-
+    
     private Map<TimeOutEvent, InterruptHandler> pendingTimeouts;
-
+    
     public void scheduleInterrupt(int timeUnits, InterruptHandler handler, Object... varargs) {
         Integer processID = (Integer)varargs[0];
         TimeOutEvent timeOut = new TimeOutEvent(timeUnits+systemTime, processID, this);
         pendingTimeouts.put(timeOut, handler);
         Config.getEventScheduler().schedule(timeOut);
     }
-
+    
     public void cancelInterrupt(int processID) {
         TimeOutEvent event = null;
         for(TimeOutEvent candidate : pendingTimeouts.keySet()) {
@@ -34,7 +34,7 @@ class SimulationClock implements EventHandler<TimeOutEvent>, SystemTimer {
         pendingTimeouts.remove(event);
         Config.getEventScheduler().cancel(event);
     }
-
+    
     public void process(TimeOutEvent event) {
         InterruptHandler handler = pendingTimeouts.get(event);
         pendingTimeouts.remove(event);
@@ -44,74 +44,80 @@ class SimulationClock implements EventHandler<TimeOutEvent>, SystemTimer {
         handler.interrupt(InterruptHandler.TIME_OUT, event.getProcessID());
         TRACE.INTERRUPT_END();
     }
-
-
+    
+    
     private long systemTime;
     private long kernelTime;
-    private long userTime;
+    private long userTime;    
+    private long idleTime;
 
     public SimulationClock(int sysCallCost, int cSwitchCost) {
         this.systemTime = 0;
         this.kernelTime = 0;
         this.userTime = 0;
+        this.idleTime = 0;
         pendingTimeouts = new HashMap<TimeOutEvent, InterruptHandler>();
 
         this.SYSCALL_COST=sysCallCost;
         this.CSWITCH_COST=cSwitchCost;
     }
-
+    
     void logContextSwitch() {
         this.advanceKernelTime(this.CSWITCH_COST);
     }
-
+    
     void logInterrupt() {
         this.advanceKernelTime(this.SYSCALL_COST);
     }
-
+    
     void logSystemCall() {
         this.advanceKernelTime(this.SYSCALL_COST);
     }
-
+    
     public void advanceKernelTime(long time) {
         assert(time>=0);
         systemTime+=time;
         kernelTime+=time;
     }
-
+    
     public void advanceSystemTime(long time) {
         assert(time>=0);
         systemTime+=time;
     }
-
+    
     public void advanceUserTime(long time) {
         assert(time>=0);
         systemTime+=time;
         userTime+=time;
     }
-
+   
     public void setSystemTime(long time) {
         assert(time>=0);
         systemTime=time;
         userTime=0;
         kernelTime=0;
     }
-
-
+    
+    
     public long getSystemTime() { return systemTime; }
     public long getKernelTime() { return kernelTime; }
     public long getUserTime() { return userTime; }
     public long getIdleTime() { return systemTime-(userTime+kernelTime); }
-
-
+   
+    
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("System time: "+this.getSystemTime()+"\n");
         stringBuilder.append("Kernel time: "+this.getKernelTime()+"\n");
         stringBuilder.append("User time: "+this.getUserTime()+"\n");
-        stringBuilder.append("Idle time: "+(this.getSystemTime()-(this.getKernelTime()+this.getUserTime()))+"\n");
-        stringBuilder.append("Context switches: "+Config.getCPU().getContextSwitches()+"\n");
-        stringBuilder.append("CPU utilization: %.2f\n"+(double)Config.getSimulationClock().getUserTime()/Config.getSimulationClock().getSystemTime()*100);
+        stringBuilder.append("Idle time: "+(this.getSystemTime()-(this.getKernelTime()+this.getUserTime())));
         return stringBuilder.toString();
     }
 
+    public void advanceIdleTime(long time) {
+        //added in this method so that i can just keep track of idle time rather than calculate it at the end
+        assert(time>=0);
+        idleTime+=time;
+        advanceSystemTime(time);
+    }
 }
