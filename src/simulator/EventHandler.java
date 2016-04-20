05d81862a780549db77b947287a0ed31eb76e8e4
Erin <erinversfeld@gmail.com>
2016-04-20 21:33:50 +0200

    [docs] gave a description of what the interface is for

--- src/simulator/EventHandler.java
+++ src/simulator/EventHandler.java
@@ -1,8 +1,7 @@
 package simulator; 
 /**
- * 
- * 
- * @author Stephan Jamieson 
+ * Interface for handling the processing of events
+ * @author Stephan Jamieson
  * @version 8/3/15
  */
 interface EventHandler<E extends Event> {
