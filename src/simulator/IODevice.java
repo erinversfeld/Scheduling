6f5a57381f9ab440342811ac4ca2e2551e734805
Erin <erinversfeld@gmail.com>
2016-04-20 16:59:18 +0200

    [refactor] remove unused methods that were probably unnecessary to create in the first place

--- src/simulator/IODevice.java
+++ src/simulator/IODevice.java
@@ -1,4 +1,4 @@
-package simulator; 
+package simulator;
 import java.util.HashMap;
 import java.util.Map;
 /**
@@ -6,46 +6,46 @@ import java.util.Map;
  * <p>
  * It is expected that an IO request is blocking i.e. the calling process will be removed from the CPU
  * while waiting for the request to complete.
- * 
+ *
  * @author Stephan Jamieson
  * @version 8/3/15
  */
 public class IODevice {
-        
+
     private int ID;
     private String name;
     private long freeTime;
-    private Map<Integer, ProcessControlBlock> queue;    
-    
+    private Map<Integer, ProcessControlBlock> queue;
+
+    public ProcessControlBlock getProcess(){
+        return queue.get(ID);
+    }
+
     /**
      * Create a device with the given id and name (e.g. 'disk').
      */
-    public IODevice(int ID, String name) { 
-        this.ID = ID; 
+    public IODevice(int ID, String name) {
+        this.ID = ID;
         this.name = name;
         this.queue = new HashMap<Integer, ProcessControlBlock>();
     }
-    
+
     /**
      * Obtain the device name.
      */
     public String getName() { return name; }
-    
+
     /**
      * Obtain the device id.
      */
     public int getID() { return ID; }
 
-    public ProcessControlBlock getProcess(){
-        return queue.get(ID);
-    }
-    
     long getFreeTime() { return freeTime; }
     void setFreeTime(long systemTime) { freeTime = systemTime; }
 
     /**
      * Make an IO request of the given duration, calling the given interrupt handler 
-	 * when the request is complete.
+     * when the request is complete.
      */
     public void requestIO(int duration, final ProcessControlBlock process, final InterruptHandler handler) {
         if (this.getFreeTime()<=Config.getSystemTimer().getSystemTime()) {
@@ -55,26 +55,26 @@ public class IODevice {
             this.setFreeTime(this.getFreeTime()+duration);
         }
         queue.put(process.getPID(), process);
-        WakeUpEvent event = new WakeUpEvent(this.getFreeTime(), this.getID(), process.getPID(), 
-            new EventHandler<WakeUpEvent>() {
-                public void process(WakeUpEvent event) {
-                    queue.remove(event.getProcessID());
-                    assert(process.hasNextInstruction());
-                    process.nextInstruction();
-                    TRACE.INTERRUPT(InterruptHandler.WAKE_UP,  deviceToString(), process);
-                    Config.getSimulationClock().logInterrupt();
-                    handler.interrupt(InterruptHandler.WAKE_UP,  getID(), process);
-                    TRACE.INTERRUPT_END();
-    
-                }                            
-            });
+        WakeUpEvent event = new WakeUpEvent(this.getFreeTime(), this.getID(), process.getPID(),
+                new EventHandler<WakeUpEvent>() {
+                    public void process(WakeUpEvent event) {
+                        queue.remove(event.getProcessID());
+                        assert(process.hasNextInstruction());
+                        process.nextInstruction();
+                        TRACE.INTERRUPT(InterruptHandler.WAKE_UP,  deviceToString(), process);
+                        Config.getSimulationClock().logInterrupt();
+                        handler.interrupt(InterruptHandler.WAKE_UP,  getID(), process);
+                        TRACE.INTERRUPT_END();
+
+                    }
+                });
         Config.getEventScheduler().schedule(event);
     }
 
     private String deviceToString() { return this.toString(); }
-	
-	/**
-	 * Obtain a string representation of this device of the form 'device(id=<id>)'.
-	 */
+
+    /**
+     * Obtain a string representation of this device of the form 'device(id=<id>)'.
+     */
     public String toString() { return "device(id="+this.getID()+")"; }
 }
