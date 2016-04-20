46dccfb467498509c87d7e8aefa75fa951ff86f6
Erin <erinversfeld@gmail.com>
2016-04-20 21:32:00 +0200

    [refactor] ditto

--- src/simulator/CPUInstruction.java
+++ src/simulator/CPUInstruction.java
@@ -1,8 +1,6 @@
 package simulator; 
 /**
  * Abstract representation of a block of program code describing a cpu burst.
- * 
- * 
  * @author Stephan Jamieson
  * @version 8/3/15
  */
@@ -17,14 +15,12 @@ public class CPUInstruction extends Instruction {
     int getBurstRemaining() { return burstRemaining; }
     
     /**
-     * Simulate execution of cpu burst for <code>timeUnits</code> time.
-     * 
+     * Simulate execution of cpu burst for a given amount of time.
      * If this cpu burst can complete in the given time, the method returns the quantity
      * of unused time units.
      * If this burst cannot complete in the given time then the method returns 
      * a -ve value, n,  where -n represents the amount of burst time remaining.
-     * 
-     * @return <code>timeUnits-this.getBurstRemaining()</code>.
+     * @return the time remaining for the instruction to execute.
      */
     int execute(int timeUnits){ 
         int remainder = timeUnits-burstRemaining;
@@ -36,10 +32,8 @@ public class CPUInstruction extends Instruction {
         
     /**
      * Simulate execution of cpu burst to completion.
-     * <p>
      * The method returns the quantity of time units consumed.
-     * 
-     * @return <code>this.getDuration()</code>.
+     * @return the time remaining for the instruction to execute.
      */
     int execute() {
 		final int period = this.burstRemaining;
