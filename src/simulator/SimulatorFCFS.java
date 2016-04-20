f0f53da5289bbade86a92f039e07aa56bac5a847
Erin <erinversfeld@gmail.com>
2016-04-20 17:04:02 +0200

    [refacor] renamed some variables to make sense and took out unused ones

--- src/simulator/SimulatorFCFS.java
+++ src/simulator/SimulatorFCFS.java
@@ -9,20 +9,20 @@ public class SimulatorFCFS {
     public static void main(String[] args){
         //Read info from screen/user
         System.out.println("*** FCFS Simulator ***");
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         //File name
        print("Enter configuration file name: ");
-        String config_filename = scan.nextLine();
+        String config_filename = input.nextLine();
         //System call cost
        print("Enter cost of system call: ");
-        int cost_syscall = scan.nextInt();
+        int cost_syscall = input.nextInt();
         //Content switch cost
         System.out.print("Enter cost of context switch: ");
-        int cost_context_switch = scan.nextInt();
+        int cost_context_switch = input.nextInt();
         //Trace level
        print("Enter trace level: ");
-        int trace_level = scan.nextInt();
-        scan.close();
+        int trace_level = input.nextInt();
+        input.close();
 
         //Determine if there is a trace level, if so print trace
         if (trace_level>0){
@@ -30,8 +30,6 @@ public class SimulatorFCFS {
         }
 
         //init eventQ, SystemTimer and kernel
-        EventQueue eventQueue = new EventQueue();
-        SystemTimerImpl systemTimer = new SystemTimerImpl();
         Kernel kernel = new FCFSKernel();
 
         //init trace
