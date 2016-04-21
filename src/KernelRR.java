2c111d507d99be3441b48a4d66ec6128bbe9e322
Erin <erinversfeld@gmail.com>
2016-04-21 17:10:27 +0200

    [docs[ ditto. same doc strings as same work effectively being done, just different algorithm applied.

--- src/KernelRR.java
+++ src/KernelRR.java
@@ -6,8 +6,6 @@ import java.util.LinkedList;
 
 import static simulator.ProcessControlBlock.State.*;
 
-//
-
 /**
  * Concrete Kernel type for the execution of a round robin simulation of process scheduling in an operating system.
  */
@@ -16,10 +14,18 @@ public class KernelRR implements Kernel {
     private LinkedList<ProcessControlBlock> readyQueue = new LinkedList<ProcessControlBlock>();;
     private int slice;
 
+    /**
+     * Constructor method for KernelRR
+     * @param slice
+     */
     public KernelRR(int slice) {
         this.slice = slice;
     }
 
+    /**
+     * Puts a new process onto the eventQ, performing a context switch to do so
+     * @return the process taken off of the q
+     */
     private ProcessControlBlock dispatch() {
         CPU cpu = Config.getCPU();
         ProcessControlBlockImpl prev_process = (ProcessControlBlockImpl) cpu.getCurrentProcess();
@@ -28,16 +34,19 @@ public class KernelRR implements Kernel {
         //Checks if the ready queue isn't empty
         if(!readyQueue.isEmpty()) {
             next_process = readyQueue.poll();
+            next_process.setState(READY);
             //Switch cpu content to next setup
             cpu.contextSwitch(next_process);
-            //Set state to running
-            next_process.setState(RUNNING);
         }else{
             cpu.contextSwitch(null);
         }
         return prev_process;
 	}
 
+    /**
+     * Puts a new process onto the eventQ, performing a context switch to do so
+     * @return the process taken off of the q
+     */
     public int syscall(int number, Object... varargs) {
         int result = 0;
         CPU cpu = Config.getCPU();
@@ -51,10 +60,13 @@ public class KernelRR implements Kernel {
                 break;
             case EXECVE:
                 {
+                    //load the program into a pcb
                     ProcessControlBlock pcb = loadProgram((String)varargs[0]);
                     if (!pcb.equals(null)) {
+                        //set the priority of the pcb according to the spec given in the config file
                         pcb.setPriority((Integer)varargs[1]);
                         readyQueue.add(pcb);
+                        //execute and schedule a time to pause/halt the execution
                         if (cpu.isIdle()){
                             dispatch();
                             int pid = cpu.getCurrentProcess().getPID();
@@ -68,14 +80,17 @@ public class KernelRR implements Kernel {
                 break;
             case IO_REQUEST:
                 {
+                    //starts off the same as in fcfs
                     ProcessControlBlock pcb = cpu.getCurrentProcess();
                     IODevice ioDevice = Config.getDevice((Integer)varargs[0]);
                     ioDevice.requestIO((Integer)varargs[1], pcb, this);
                     pcb.setState(WAITING);
+                    //difference comes in here: we cancel any forthcoming interrupts
                     if(!pcb.equals(null)){
                         int pid = pcb.getPID();
                         systemTimer.cancelInterrupt(pid);
                     }
+                    //then begin execution and schedule our own one to happen
                     dispatch();
                     if(cpu.getCurrentProcess()!=null){
                         int pid = cpu.getCurrentProcess().getPID();
@@ -85,12 +100,16 @@ public class KernelRR implements Kernel {
                 break;
             case TERMINATE_PROCESS:
                 {
+                    //starts off like fcfs
                     ProcessControlBlock pcb = (ProcessControlBlock)cpu.getCurrentProcess();
                     pcb.setState(TERMINATED);
+                    //then we act as we would in io_request
                     if(!pcb.equals(null)){
                         systemTimer.cancelInterrupt(pcb.getPID());
                     }
                     dispatch();
+                    //the difference being here, where we schedule a new interrupt if there are more processes
+                    // still to share time slices with, otherwise it carries on running on of its own accord
                     if (cpu.getCurrentProcess() != null){
                         int pid = cpu.getCurrentProcess().getPID();
                         systemTimer.scheduleInterrupt(slice, this, pid);
@@ -103,19 +122,22 @@ public class KernelRR implements Kernel {
         return result;
     }
 
+    /**
+     * Kernel handler method for interrupts
+     * @param interruptType a representation of an enum which indicates which type of interrupt has occurred
+     * @param varargs other argumens which the syscall may or may not require
+     */
     public void interrupt(int interruptType, Object... varargs){
         CPU cpu = Config.getCPU();
         SystemTimer systemTimer = Config.getSystemTimer();
 
         switch (interruptType) {
             case TIME_OUT:
-                if(!Config.getCPU().isIdle()){
+                if(!cpu.isIdle()){
                     ProcessControlBlock pcb = (ProcessControlBlock)cpu.getCurrentProcess();
-                    pcb.setState(READY);
                     readyQueue.add(pcb);
                     cpu.contextSwitch(readyQueue.poll());
                     ProcessControlBlock curr = cpu.getCurrentProcess();
-                    curr.setState(RUNNING);
                     int pid = curr.getPID();
                     systemTimer.scheduleInterrupt(slice, this, pid);
                 }
@@ -123,7 +145,6 @@ public class KernelRR implements Kernel {
             case WAKE_UP:
                 ProcessControlBlock pcb = (ProcessControlBlock)varargs[1];
                 readyQueue.add(pcb);
-                pcb.setState(READY);
                 if(cpu.isIdle()){
                     dispatch();
                     int pid = cpu.getCurrentProcess().getPID();
@@ -134,7 +155,12 @@ public class KernelRR implements Kernel {
                 throw new IllegalArgumentException("KernelRR:interrupt("+interruptType+"...): unknown type.");
         }
     }
-    
+
+    /**
+     * Helper method for constructing a pcb with a program's instructions loaded into it
+     * @param filename the name of the program who's instructions are to be loaded
+     * @return a pcb representing the program
+     */
     private static ProcessControlBlock loadProgram(String filename) {
         try {
             return ProcessControlBlockImpl.loadProgram(filename);
